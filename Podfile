ea014f9037d69de7ce7b44beff35ee0a5e10729b
ss <reybravin@gmail.com>
2018-02-11 15:09:32 +0200

    last updated

--- Podfile
+++ Podfile
@@ -1,14 +1,15 @@
-project 'uber-clone.xcodeproj'
+project 'Uber.xcodeproj'
 
 # Uncomment the next line to define a global platform for your project
 # platform :ios, '9.0'
 
-target 'uber-clone' do
+target 'Uber' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
 
-  # Pods for uber-clone
+  # Pods for Uber
   pod 'Firebase/Core'
   pod 'Firebase/Auth'
+  pod 'Firebase/Database'
 
 end
---
+++ Uber/ViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ViewController.swift
+//  Uber
+//
+//  Created by zappycode on 6/16/17.
+//  Copyright © 2017 Nick Walter. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var driverLabel: UILabel!
+    @IBOutlet weak var riderLabel: UILabel!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var riderDriverSwitch: UISwitch!
+    @IBOutlet weak var topButton: UIButton!
+    @IBOutlet weak var bottomButton: UIButton!
+    
+    var signUpMode = true
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func topTapped(_ sender: Any) {
+        if emailTextfield.text == "" || passwordTextField.text == "" {
+            displayAlert(title: "Missing Information", message: "You must provide both a email and password")
+        } else {
+            if let email = emailTextfield.text {
+                if let password = passwordTextField.text {
+                    if signUpMode {
+                        // SIGN UP
+                        Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error) in
+                            if error != nil {
+                                self.displayAlert(title: "Error", message: error!.localizedDescription)
+                            } else {
+                                
+                                if self.riderDriverSwitch.isOn {
+                                    // DRIVER
+                                    let req = Auth.auth().currentUser?.createProfileChangeRequest()
+                                    req?.displayName = "Driver"
+                                    req?.commitChanges(completion: nil)
+                                    self.performSegue(withIdentifier: "driverSegue", sender: nil)
+                                } else {
+                                    // RIDER
+                                    let req = Auth.auth().currentUser?.createProfileChangeRequest()
+                                    req?.displayName = "Rider"
+                                    req?.commitChanges(completion: nil)
+                                    self.performSegue(withIdentifier: "riderSegue", sender: nil)
+                                }
+                            }
+                        })
+                    } else {
+                        // LOG IN
+                        Auth.auth().signIn(withEmail: email, password: password, completion: { (user, error) in
+                            if error != nil {
+                                self.displayAlert(title: "Error", message: error!.localizedDescription)
+                            } else {
+                                if user?.displayName == "Driver" {
+                                    // DRIVER
+                                    self.performSegue(withIdentifier: "driverSegue", sender: nil)
+                                } else {
+                                    // RIDER
+                                    self.performSegue(withIdentifier: "riderSegue", sender: nil)
+                                }
+                            }
+                        })
+                    }
+                }
+            }
+        }
+    }
+    
+    func displayAlert(title:String, message:String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func bottomTapped(_ sender: Any) {
+        if signUpMode {
+            topButton.setTitle("Log In", for: .normal)
+            bottomButton.setTitle("Switch to Sign Up", for: .normal)
+            riderLabel.isHidden = true
+            driverLabel.isHidden = true
+            riderDriverSwitch.isHidden = true
+            signUpMode = false
+        } else {
+            topButton.setTitle("Sign Up", for: .normal)
+            bottomButton.setTitle("Switch to Log In", for: .normal)
+            riderLabel.isHidden = false
+            driverLabel.isHidden = false
+            riderDriverSwitch.isHidden = false
+            signUpMode = true
+        }
+    }
+    
+}
+
